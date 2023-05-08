5f1e556fb3521b726e49365d50c0514bd1d64a4e
Syedammar515 <syedammar515@gmail.com>
2023-05-08 04:32:51 +0200

    hydra bis

--- sawyer_VR_controller/scripts/test_gpt.py
+++ sawyer_VR_controller/scripts/test_gpt.py
@@ -1,13 +1,19 @@
+#!/usr/bin/env python
+
 import rospy
 import sys
 import moveit_commander
 from geometry_msgs.msg import Pose, Point
 from std_msgs.msg import Float32MultiArray
 import threading
+from razer_hydra.msg import Hydra
+
 
 def hydra_callback(msg):
     global target_position
-    target_position = msg.data
+    xyz = msg.paddles[1].transform.translation
+    target_position=xyz
+
 
 def main():
     global target_position
@@ -17,28 +23,34 @@ def main():
     rospy.init_node('sawyer_hydra_follow_moveit')
 
     # Subscribe to the 'hydra_calib' topic
-    rospy.Subscriber("hydra_calib", Float32MultiArray, hydra_callback)
+    rospy.Subscriber("hydra_calib", Hydra, hydra_callback)
 
     # Initialize the robot and MoveGroupCommander for the arm
     robot = moveit_commander.RobotCommander()
     group = moveit_commander.MoveGroupCommander("right_arm")
+    group.set_max_velocity_scaling_factor(0.5)
 
-    rate = rospy.Rate(100)  # 100 Hz update rate
+    rate = rospy.Rate(20)  # 100 Hz update rate
 
     while not rospy.is_shutdown():
+        
         if target_position is not None:
+            rospy.logwarn(target_position.x)
             # Convert Hydra position to a Pose message
-            target_pose = Pose()
-            target_pose.position.x = target_position[0]
-            target_pose.position.y = target_position[1]
-            target_pose.position.z = target_position[2]
 
             # Set target pose and plan
-            group.set_pose_target(target_pose)
+            group.set_position_target([target_position.x,target_position.y,target_position.z])
             plan = group.plan()
 
             # Execute the plan
-            group.execute(plan)
+            group.execute(plan, wait=True)
+
+            if plan.joint_trajectory.points:
+                rospy.logerr("Executing plan")
+                # Execute the plan
+                group.execute(plan)
+            else:
+                rospy.logerr("Failed to find a valid plan")
 
         rate.sleep()
 
